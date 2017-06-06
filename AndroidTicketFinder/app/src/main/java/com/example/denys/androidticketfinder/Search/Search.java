546777e1b961ebe10cec0a08e2ba67504cdbf8be
dburtnja <dostup.te@gmail.com>
2017-06-06 18:02:02 +0300

    add find tillDate

--- AndroidTicketFinder/app/src/main/java/com/example/denys/androidticketfinder/Search/Search.java
+++ AndroidTicketFinder/app/src/main/java/com/example/denys/androidticketfinder/Search/Search.java
@@ -16,6 +16,13 @@ import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.webkit.WebView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.denys.androidticketfinder.Main2Activity;
 import com.example.denys.androidticketfinder.MainActivity;
 import com.example.denys.androidticketfinder.Search.train_search.Post;
@@ -28,22 +35,26 @@ import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class Search{
-    private static long DAY = 86400000;
-    private Context context;
-    private Ticket ticket;
-    private SimpleDateFormat simpleDate;
-    private SimpleDateFormat simpleTime;
+    private static long         DAY = 86400000;
+    private Context             context;
+    private Ticket              ticket;
+    private SimpleDateFormat    simpleDate;
+    private SimpleDateFormat    simpleTime;
+    private RequestQueue        queue;
 
     public Search(Context context, Ticket ticket) {
         this.context = context;
         this.ticket = ticket;
         this.simpleDate = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
         this.simpleTime = new SimpleDateFormat("HH:mm", Locale.getDefault());
+        this.queue = Volley.newRequestQueue(context);
     }
 
     public void findTicket() {
@@ -58,12 +69,51 @@ public class Search{
     }
 
     private void checkForTrain(Ticket ticket) {
-        final Post  post;
-        String      searchParam;
-        SelectTrain selectTrain;
+        String          searchParam;
+        String          url;
+        SelectTrain     selectTrain;
+        StringRequest   postRequest;
 
-        post = new Post();
-        searchParam = getSearchParam(ticket);
+        url = "http://booking.uz.gov.ua/purchase/search/";
+        postRequest = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("SEARCH.Response", response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.e("SEARCH.Error", error.getMessage());
+                    }
+                }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params;
+                Date                fromDate;
+                Date                tillDate;
+                Ticket              ticket;
+
+                ticket = Search.this.ticket;
+                params = new HashMap<>();
+                fromDate = new Date(ticket.fromDate);
+                tillDate = new Date(ticket.tillDate);
+                params.put("station_id_from", ticket.fromStation.value + "");
+                params.put("station_id_till", ticket.tillStation.value + "");
+                params.put("station_from", "");
+                params.put("station_till", "");
+                params.put("date_dep", simpleDate.format(fromDate));
+                params.put("time_dep", simpleTime.format(fromDate));
+                params.put("time_dep_till", simpleTime.format(tillDate));
+                params.put("another_ec", "0");
+                params.put("search", "");
+                return params;
+            }
+        };
+        queue.add(postRequest);
+
+        /*
         Object obj = post.sendPost("http://booking.uz.gov.ua/purchase/search/", searchParam,
                 TrainSearch.class, ticket);
         if (obj != null) {
@@ -74,14 +124,14 @@ public class Search{
                 MediaPlayer player = MediaPlayer.create(context, notification);
                 player.setLooping(true);
                 player.start();
-    /*            Intent intent1 = new Intent(this, MainActivity.class);
+                Intent intent1 = new Intent(this, MainActivity.class);
                 intent1.putExtra("_gv_sessid", ticket.cookie);
                 intent1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent1);*/
+                startActivity(intent1);
             }
             else
                 onCheckNotification();
-        }
+        }*/
     }
 
     private long getNextDayTime(long inputTime) {
@@ -99,20 +149,6 @@ public class Search{
             return (-1);
         else
             return (nextDayDate.getTime());
-    }
-
-    private String getSearchParam(Ticket ticket) {
-        Date fromData = new Date(ticket.fromDate);
-        Date tillData = new Date(ticket.tillDate);
-        return "station_id_from=" + ticket.fromStation.value +
-                "&station_id_till=" + ticket.tillStation.value +
-                "&station_from=" +
-                "&station_till=" +
-                "&date_dep=" + simpleDate.format(fromData) +
-                "&time_dep=" + simpleTime.format(fromData) +
-                "&time_dep_till=" + simpleTime.format(tillData) +
-                "&another_ec=0" +
-                "&search=";
     }
 
     private void onCheckNotification() {
