663c4ef4c91b266b0620b1511949a0df86024eea
dburtnja <dostup.te@gmail.com>
2017-07-04 17:09:25 +0300

    start ticketFinder1.0

--- AndroidTicketFinder1.0/app/src/main/java/com/example/dburtnja/androidticketfinder10/MainActivity.java
+++ AndroidTicketFinder1.0/app/src/main/java/com/example/dburtnja/androidticketfinder10/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.dburtnja.androidticketfinder10;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.example.dburtnja.androidticketfinder10.TicketInfo.Ticket;
 
 public class MainActivity extends AppCompatActivity {
+    private Ticket          ticket;
+    private EditText        getStationFrom;
+    private EditText        getStationTill;
+    private ImageButton     getReplaceStations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ticket = new Ticket(this);
+        getStationFrom = (EditText) findViewById(R.id.stationFrom);
+        getStationTill = (EditText) findViewById(R.id.stationTill);
+        getReplaceStations = (ImageButton) findViewById(R.id.replaceStations);
 
+        getStationFrom.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus && !getStationFrom.getText().toString().equals("")) {
+                    ticket.setStationFrom(getStationFrom);
+                }
+            }
+        });
+
+        getStationTill.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus && !getStationTill.getText().toString().equals("")) {
+                    ticket.setStationFrom(getStationTill);
+                }
+            }
+        });
     }
+
+
 }
---
+++ AndroidTicketFinder1.0/app/src/main/java/com/example/dburtnja/androidticketfinder10/TicketInfo/Ticket.java
@@ -0,0 +1,113 @@
+package com.example.dburtnja.androidticketfinder10.TicketInfo;
+
+import android.content.Context;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * Created by dburtnja on 04.07.17.
+ */
+
+public class Ticket {
+    private Context         context;
+    private RequestQueue    queue;
+    private Station         stationFrom;
+    private Station         stationTill;
+
+    public Ticket(Context context) {
+        this.context = context;
+        this.queue = Volley.newRequestQueue(context);
+    }
+
+    public Station getStationFrom() {
+        if (stationFrom != null)
+            return stationFrom;
+        return stationFrom;
+    }
+
+    public Station getStationTill() {
+        if (stationTill != null)
+            return stationTill;
+        return stationTill;
+    }
+
+    public void setStationFrom(EditText stationName) {
+        this.stationFrom = new Station(stationName, queue, context);
+    }
+
+    public void setStationTill(EditText stationName) {
+        this.stationTill = new Station(stationName, queue, context);
+    }
+
+    private class Station {
+        private String  title;
+        private String  region;
+        private int     value;
+
+        public Station(final EditText stationName, RequestQueue queue, final Context context) {
+            JsonArrayRequest    getRequest;
+            String              url;
+
+            url = "http://booking.uz.gov.ua/mobile/train_search/station/?term=";
+            try {
+                url = url + URLEncoder.encode(String.valueOf(stationName.getText()), "UTF-8");
+            } catch (UnsupportedEncodingException e) {
+                toast("Помилка кодування станції", context, true);
+                e.printStackTrace();
+            }
+            getRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                    new Response.Listener<JSONArray>() {
+                        @Override
+                        public void onResponse(JSONArray arrayResponse) {
+                            JSONObject  response;
+
+                            try {
+                                response = arrayResponse.getJSONObject(0);
+                                title = response.getString("title");
+                                region = response.getString("region");
+                                value = response.getInt("value");
+                            } catch (JSONException e) {
+                                toast("Помилка отримання станції", context, true);
+                                e.printStackTrace();
+                            }
+                            stationName.setText(title);
+                            toast("Станція: " + title + ". Значення: " + value, context, false);
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            toast("Помилка отримання даних станції", context, true);
+                            Log.e("STATION_ERROR", error.getMessage());
+                        }
+                    });
+            queue.add(getRequest);
+        }
+
+        public void toast(String msg, Context context, Boolean vibrate) {
+            Toast.makeText(context, msg, Toast.LENGTH_LONG).show();
+            if (vibrate) {
+                Vibrator    vibrator;
+
+                vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);;
+                vibrator.vibrate(500);
+            }
+        }
+    }
+}
